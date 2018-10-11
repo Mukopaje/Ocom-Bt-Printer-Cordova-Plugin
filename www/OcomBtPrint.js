e335e8c5c57172b8b133d218f2c1fde697ae9603
Mukopaje <mukopaje@outsourcenow.us>
2018-10-11 12:20:07 +0200

    Initial Push

---
+++ www/OcomBtPrint.js
@@ -0,0 +1,9 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'OcomBtPrint', 'coolMethod', [arg0]);
+};
+
+exports.bTPrintTest = function (arg0, success, error) {
+    exec(success, error, 'OcomBtPrint', 'bTPrintTest', [arg0]);
+};
